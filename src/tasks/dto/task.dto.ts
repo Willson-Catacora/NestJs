50592b3774da9428d6644e486cf21f29d0d9c63a
willson <willsoncatacora1@gmail.com>
2024-01-18 19:13:25 -0400

    Validacion con las class-validator

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,20 @@
 import { TaskStatus } from "../task.entity";
+import { IsIn, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateTaskDto{
+    @IsString()
+    @IsNotEmpty()
     title: string;
+    @IsString()
     description: string;
 }
 
 export class UpdateTaskDto{
+    @IsString()
     title?: string;
+    @IsString()
     description?: string;
+    @IsString()
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.PENDING])
     status: TaskStatus;
 }
